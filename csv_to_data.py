d4286980dd50225d62964a9db2aedbc79be0a37a
Ander Garmendia <agarmendia@countercraft.eu>
2020-02-06 22:57:10 +0100

    Parametrize script

--- csv_to_data.py
+++ csv_to_data.py
@@ -1,26 +1,38 @@
 import csv
+import couchdb
+import json
+import sys
+
 from estropadakparser.estropada.estropada import Estropada, TaldeEmaitza
 from operator import attrgetter
 
+couch = couchdb.Server('http://admin:HHZ60Et4wxJLhLs@127.0.0.1:5984')
+db = couch['test']
+
 kategoriak = ['Haurra', 'Infantila', 'Promesa', 'Kadete', 'Jubenil', 'Absolut', 'Jubenil', 'Senior']
 
-def print_pretty(estropada: Estropada):
-    sailkapena = sorted(estropada.sailkapena, key=attrgetter('posizioa'))
-    for talde in sorted(sailkapena, key=attrgetter('tanda')):
-        print(u'{0:<6}\t{1:^5}\t{2:^5}\t{3:<30}\t{4:<25}\t{5:<8}\t{6:<12}'.format(
-                    str(talde.posizioa), talde.tanda, talde.kalea,
-                    talde.talde_izena, u'\t'.join(talde.ziabogak),
-                    talde.denbora, talde.kategoria))
-
-
 if __name__ == "__main__":
+    lekua = None
+    id = None
+    filename = sys.argv[1]
+    izena = sys.argv[2]
+    if sys.argv[3]:
+        id = sys.argv[3]
+    if sys.argv[4]:
+        lekua = sys.argv[4]
     with open('./data/2020111_194026_200111_Arraun denborrak.csv', newline='') as csvfile:
         teamreader = csv.reader(csvfile, delimiter=",")
 
-        estropada = Estropada('Gipuzkoako batel liga, 1. Jardunaldia', **{'data': '2020-01-11 15:30', 'lekua': 'Martutene (Donostia)'})
+        e = {'izena': izena}
+        if id:
+            e = db[id]
+        if lekua:
+            e['lekua'] = lekua
+        estropada = Estropada(**e)
 
         tanda = 0
         kategoria_base = None
+        kategoriak_helper = []
         for row in teamreader:
             posizioa = None
             try:
@@ -31,8 +43,21 @@ if __name__ == "__main__":
                 kategoria_base = kategoriak[tanda]
                 tanda = tanda + 1
             kategoria = kategoria_base + ' ' + row[3]
-            t = TaldeEmaitza(row[2], **{'tanda': tanda, 'kalea': 1, 'posizioa': posizioa, 'kategoria': kategoria, 'denbora':row[8], 'puntuak': row[10] })
+            kategoriak_helper.append(kategoria)
+            emaitza = {'tanda': tanda,
+                       'tanda_postua': posizioa,
+                       'kalea': 1,
+                       'posizioa': posizioa,
+                       'kategoria': kategoria,
+                       'denbora': row[8],
+                       'puntuazioa': row[10]}
+            t = TaldeEmaitza(row[2], **emaitza)
+            t.ziaboga_gehitu(row[5])
             estropada.taldeak_add(t)
+        estropada.kategoriak = list(set(kategoriak_helper))
 
-    print_pretty(estropada)
-
+    print(estropada.get_json())
+    estropada_json = estropada.get_json()
+    estropada_obj = json.loads(estropada_json)
+    estropada_obj['_rev'] = estropada._rev
+    db[estropada.id] = estropada_obj
