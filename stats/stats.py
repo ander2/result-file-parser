a4bc78d8b71ecad7b04adc26499c33ef2a3e4e86
Ander Garmendia <agarmendia@countercraft.eu>
2020-04-14 08:18:44 +0200

    Add functions to create DataFrames

--- stats/stats.py
+++ stats/stats.py
@@ -31,7 +31,7 @@ class Stats:
         stats_dao.set_sailkapen_orokorra(league, date, _category, rank)
 
     def calculate_posizioak(self, liga, urtea, kategoria):
-        '''Calculate position array for every team'''
+        '''Calculate position dict for every team'''
         puntuazioa = {}
         estropadak = stats_dao.get_estropadak(liga, urtea)
         for estropada in estropadak:
@@ -52,6 +52,27 @@ class Stats:
             except KeyError:
                 print("No sailkapena for {}".format(estropada['izena']))
         return puntuazioa
+
+    def calculate_posizioak_per_race(self, liga, urtea, kategoria):
+        '''Calculate positions array for every team'''
+        posizioak = []
+        estropadak = stats_dao.get_estropadak(liga, urtea)
+        for estropada in estropadak:
+            try:
+                sailkapena = estropada['sailkapena']
+                talde_sailkapenak = {}
+                for emaitza in sailkapena:
+                    if kategoria != emaitza['kategoria']:
+                        continue
+                    try:
+                        taldea = emaitza['talde_izena']
+                    except KeyError:
+                        print("No name for {}".format(emaitza['talde_izena']))
+                    talde_sailkapenak[taldea] = emaitza['posizioa']
+                posizioak.append(talde_sailkapenak)
+            except KeyError:
+                print("No sailkapena for {}".format(estropada['izena']))
+        return posizioak
 
     def calculate_cumulative(self, liga, urtea, kategoria):
         ''' Calculate cumulative points in rank'''
@@ -76,6 +97,31 @@ class Stats:
                         puntuazioa[taldea].append(puntuak)
                     else:
                         puntuazioa[taldea] = list([puntuak_])
+            except KeyError:
+                print("No sailkapena for {}".format(estropada['izena']))
+        return puntuazioa
+
+    def calculate_points_per_race(self, liga, urtea, kategoria):
+        ''' Calculate points per race'''
+        puntuazioa = []
+        estropadak = stats_dao.get_estropadak(liga, urtea)
+        for estropada in estropadak:
+            logging.info("%s" % estropada['izena'])
+            try:
+                sailkapena = estropada['sailkapena']
+                talde_puntuazioa = {}
+                for emaitza in sailkapena:
+                    if kategoria != emaitza['kategoria']:
+                        continue
+                    taldea = emaitza['talde_izena']
+                    if emaitza['puntuazioa'] == '':
+                        puntuak_ = 0
+                    else:
+                        puntuak_ = int(emaitza['puntuazioa'])
+                    if 'puntuazioa-rank' in emaitza:
+                        puntuak_ = int(emaitza['puntuazioa-rank'])
+                    talde_puntuazioa[taldea] = puntuak_
+                puntuazioa.append(talde_puntuazioa)
             except KeyError:
                 print("No sailkapena for {}".format(estropada['izena']))
         return puntuazioa
